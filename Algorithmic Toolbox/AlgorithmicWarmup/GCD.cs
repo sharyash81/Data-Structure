c8381c7895a8d75abd386652a183a92f5d23110a
sharyash81 <sharyash81@gmail.com>
2022-09-06 06:58:04 +0430

    Euclidean algorithm for greatest common divisor

---
+++ Algorithmic Toolbox/AlgorithmicWarmup/GCD.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace A3
+{
+    public class Program
+    {
+        public  static void Main()
+        {
+            string [] s = Console.ReadLine().Split();
+            long a = long.Parse(s[0]);
+            long b = long.Parse(s[1]);
+            Console.WriteLine(Solve(a,b));
+        }
+        public static long Solve(long a, long b)
+        {
+            return gcd(a,b);
+        }
+        public static long gcd(long a , long b)
+        {
+            if (b==0) return a;
+	        else return gcd(b,a%b);
+        }
+        
+    }
+}
