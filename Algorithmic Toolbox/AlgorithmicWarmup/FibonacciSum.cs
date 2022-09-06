b59f27585d6c04f5a0f116cc16d760205f13716c
sharyash81 <sharyash81@gmail.com>
2022-09-06 07:00:46 +0430

    Last digit of the sum of Fibonacci Numbers

---
+++ Algorithmic Toolbox/AlgorithmicWarmup/FibonacciSum.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace A3
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            long n = long.Parse(Console.ReadLine());
+            Console.WriteLine(Solve(n));
+        }
+        public static long Solve(long n)
+        {
+            return (pissano() * (n / 60) + pissano(n % 60)) % 10;
+        }
+        public static long pissano(long m = 60)
+        {
+            if (m < 3) return m;
+            long i = 1,x = 0 , y = 1 , z = 1 ;
+            long total = x + y + z;
+            while (z != 1 || y != 0)
+            {
+                i++;
+                x = y;
+                y = z;
+                z = (x + y) % 10;
+                total += z;
+                if (m - 1 == i) break;
+            }
+            return total;
+        }
+    }
+}
