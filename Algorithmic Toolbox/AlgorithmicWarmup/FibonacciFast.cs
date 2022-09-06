929061a504a42d5d6d1296b55d6ea2a20b83aa4e
sharyash81 <sharyash81@gmail.com>
2022-09-06 06:56:25 +0430

    find the nth fibonacci number

---
+++ Algorithmic Toolbox/AlgorithmicWarmup/FibonacciFast.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace A3
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int n =  int.Parse(Console.ReadLine());
+            Console.WriteLine(Solve(n));
+        }
+        public static long Solve(long n)
+        {
+            if(n < 1) return 0;
+            long [] fib_array = new long[n+1];
+            fib_array[0] = 0;
+            fib_array[1] = 1;
+            for (int i = 2 ; i <= n;i++)   fib_array[i] = fib_array[i-1] + fib_array[i-2];
+            return fib_array[n];
+        }
+   }
+}
