210cce53c82c3815d80bed89650b85d22d92c4fb
sharyash81 <sharyash81@gmail.com>
2022-09-08 12:36:52 +0430

    Changing money to 10-5-1 coins

---
+++ Algorithmic Toolbox/Greedy Algorithms/ChangingMoney.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace hw3_q1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(Number_of_coins(n));
+        }
+        public static int Number_of_coins(int num)
+        {
+            int ten = num / 10;
+            num %= 10;
+            int five = num / 5;
+            num %= 5;
+            int one = num;
+            return ten + five + one;
+        }
+    }
+}
