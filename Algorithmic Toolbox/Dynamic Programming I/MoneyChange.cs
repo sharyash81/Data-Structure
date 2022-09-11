b87f575198994fcd02e8eb036e3d2841b2322c0b
sharyash81 <sharyash81@gmail.com>
2022-09-11 23:11:45 +0430

    minimum number of 1, 3 and 4 coins - exchange money

---
+++ Algorithmic Toolbox/Dynamic Programming I/MoneyChange.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace A6
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            long n = long.Parse(Console.ReadLine());
+            Console.WriteLine(Solve(n));
+        }
+        public static long Solve(long n)
+        {
+            if (n < 3) return n;
+            if (n == 3) return 1;
+            if (n == 4) return 1;
+            long[] dp = new long[n + 1];
+            dp[0] = 0;
+            dp[1] = 1;
+            dp[2] = 2;
+            dp[3] = 1;
+            dp[4] = 1;
+            for (int i = 5; i <= n; i++)
+            {
+                dp[i] = Min(dp[i - 1], dp[i - 3], dp[i - 4]) + 1;
+            }
+            return dp[n];
+        }
+        public static long Min(params long[] a)
+        {
+            long min = long.MaxValue;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] < min) min = a[i];
+            }
+            return min;
+        }
+    }
+}
